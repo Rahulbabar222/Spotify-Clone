2101bee593295ed90b6eef27489f065d0d5ffa3d
Rahul Babar <rahulbabar222@gmail.com>
2025-02-24 11:01:25 +0530

    Made Seekbar Draggable

--- js/script.js
+++ js/script.js
@@ -271,9 +271,8 @@ async function main() {
     });
 
     //Rendering volume button
-    document.querySelector(".volumeBar").addEventListener("change", (e) => {
+    document.querySelector(".volumeBar").addEventListener("input", (e) => {
         currentSong.volume = (e.target.value) / 100;
-        console.log(`Volume set to: ${e.target.value} / 100`);
         if (e.target.value == 0) {
             document.querySelector(".mute-unmute").innerHTML = `<svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
                         <path d="M14 14.8135V9.18646C14 6.04126 14 4.46866 13.0747 4.0773C12.1494 3.68593 11.0603 4.79793 8.88232 7.02192C7.75439 8.17365 7.11085 8.42869 5.50604 8.42869C4.10257 8.42869 3.40084 8.42869 2.89675 8.77262C1.85035 9.48655 2.00852 10.882 2.00852 12C2.00852 13.118 1.85035 14.5134 2.89675 15.2274C3.40084 15.5713 4.10257 15.5713 5.50604 15.5713C7.11085 15.5713 7.75439 15.8264 8.88232 16.9781C11.0603 19.2021 12.1494 20.3141 13.0747 19.9227C14 19.5313 14 17.9587 14 14.8135Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -321,18 +320,18 @@ async function main() {
     currentSong.addEventListener("timeupdate", () => {
         document.querySelector(".timeLapsed").innerHTML = secondsToMinutesSeconds(currentSong.currentTime);
         document.querySelector(".totalTime").innerHTML = secondsToMinutesSeconds(currentSong.duration);
-        document.querySelector(".seeker").style.left = (currentSong.currentTime / currentSong.duration) * 100 + `%`;
     });
 
     //seekbar rendring
-    let seekbar = document.querySelector(".seekbar");
-    seekbar.addEventListener("click", e => {
-        //Element.getBoundingClientRect() method returns a DOMRect object providing information about the size 
-        // of an element and its position relative to the viewport.
-        //offsetX is value of x of click relative to width of seek bar
-        let seekbar_val = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
-        document.querySelector(".seeker").style.left = seekbar_val + "%";
-        currentSong.currentTime = (seekbar_val * currentSong.duration) / 100;
+    const seekbar= document.querySelector(".Seekbar");
+
+    currentSong.addEventListener("timeupdate", () => {
+        seekbar.value = (currentSong.currentTime / currentSong.duration) * 100;
+    })
+
+    seekbar.addEventListener("input",e=>{
+        
+        currentSong.currentTime= (e.target.value * currentSong.duration)/100;
     });
 
     //placeholder update for search bar upon smaller screen size
